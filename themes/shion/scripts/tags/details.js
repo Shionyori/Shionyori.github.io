56d45601777cc9156848f123df876ae1e4991a19
Shion <2637594835@qq.com>
2026-06-10 20:41:59 +0800

    refactor: use theme as git submodule

--- themes/shion/scripts/tags/details.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.registerDetailsTag = registerDetailsTag;
-function registerDetailsTag(hexo) {
-    hexo.extend.tag.register('details', function (args, content) {
-        const summary = args[0] || 'Details';
-        return `<details class="tag-details"><summary>${summary}</summary>${content}</details>`;
-    }, { ends: true });
-}
